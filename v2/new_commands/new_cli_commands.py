f54229d2d4b2d7005b3cfdc38582991c5f9a3559
N3xTr1Me <destriker17@gmail.com>
2023-02-28 03:22:37 +0300

    v2 - config file support added

--- v2/new_commands/new_cli_commands.py
+++ v2/new_commands/new_cli_commands.py
@@ -2,18 +2,23 @@ import click
 
 
 @click.pass_context
-def move(ctx):
+def save(ctx, arguments):
+    ctx.obj[ctx.obj["iteration"]] = arguments
+    ctx.obj["saved"].append(ctx.obj["iteration"])
+    ctx.obj["iteration"] += 1
+
+
+def move():
     click.echo("Shift transformation:")
 
     x = click.prompt('x-axis shift value', type=float)
     y = click.prompt('y-axis shift value', type=float)
     z = click.prompt('z-axis shift value', type=float)
 
-    ctx.obj["exec"].append({"type": "move", "coords": (x, y, z)})
+    save({"type": "move", "coords": [x, y, z]})
 
 
-@click.pass_context
-def rotate(ctx):
+def rotate():
     click.echo("Rotate transformation:")
 
     deg_rad = click.prompt('Value type', type=click.Choice(["Degree", "Radian"]))
@@ -21,11 +26,10 @@ def rotate(ctx):
     y = click.prompt('y-axis rotation angle', type=int)
     z = click.prompt('z-axis rotation angle', type=int)
 
-    ctx.obj["exec"].append({"type": "rotate", "mode": deg_rad, "coords": (x, y, z)})
+    save({"type": "rotate", "mode": deg_rad, "coords": [x, y, z]})
 
 
-@click.pass_context
-def cut(ctx):
+def cut():
     click.echo("Cut operation:")
 
     num = click.prompt("Number of operations", type=int)
@@ -36,20 +40,18 @@ def cut(ctx):
         corners.append(click.prompt("Choose a corner to cut",
                                     type=click.Choice(["up-left", "up-right", "down-left", "down-right"])))
 
-    ctx.obj["exec"].append({"type": "cut", "corners": corners})
+    save({"type": "cut", "corners": corners})
 
 
-@click.pass_context
-def patch(ctx):
+def patch():
     click.echo("Patch operation:")
 
     degree = click.prompt("Degree of interpolation", type=int)
 
-    ctx.obj["exec"].append({"type": "patch", "degree": degree})
+    save({"type": "patch", "degree": degree})
 
 
-@click.pass_context
-def remove(ctx):
+def remove():
     click.echo("Removal operation:")
 
     height = click.prompt("height value", type=float)
@@ -57,16 +59,15 @@ def remove(ctx):
     above_below = click.prompt("Remove all points above or below the given height",
                                type=click.Choice(["Above", "Below"]))
 
-    ctx.obj["exec"].append({"type": "remove", "height": height, "mode": above_below})
+    save({"type": "remove", "height": height, "mode": above_below})
 
 
-@click.pass_context
-def mount(ctx):
+def mount():
     click.echo("Mount operation")
 
     path = click.prompt("Path to GeoTIFF picture to mount", type=click.Path(exists=True))
 
-    ctx.obj["exec"].append({"type": "mount", "path": path})
+    save({"type": "mount", "path": path})
 
 
 operations = {"move": move,
